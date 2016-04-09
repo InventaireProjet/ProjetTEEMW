c28e2b506c51d5a608b15c03377ef1ec4a76cf4c
InventaireProjet <poubwd@gmail.com>
2016-04-09 12:30:41 +0200

    Mise à jour dossier

---
+++ Controller/login.php
@@ -0,0 +1,101 @@
+<?php
+require_once '../Model/class.MySqlManager.php';
+include_once '../Vue/header.inc';
+
+$mysql = new MySqlManager ();
+
+if (isset ( $_POST ['action'] )) {
+	if ($_POST ['action'] == 'Register') {
+		register ( $mysql );
+	}
+	
+	if ($_POST ['action'] == 'Login') {
+		authenticate ( $mysql );
+	}
+}
+if (isset ( $_GET ['action'] )) {
+	if ($_GET ['action'] == 'logout') {
+		logout ();
+	}
+}
+function authenticate($mysql) {
+	$uname = $_POST ['usr'];
+	$pwd = $_POST ['pwd'];
+	$result = $mysql->checkLogin ( $uname, $pwd );
+	if (! $result) {
+		$_SESSION ['form_data'] = array (
+				$uname,
+				$pwd 
+		);
+		$_SESSION ['msg'] = 'Username or password incorrect';
+		header ( "location:index.php" );
+		exit ();
+	}
+	$_SESSION ['msg'] = 'Welcome ' . $result->firstname . ' ' . $result->lastname;
+	$_SESSION ['user'] = $result;
+	header ( "location:index.php" );
+	exit ();
+}
+function logout() {
+	session_destroy ();
+	header ( "location:index.php" );
+	exit ();
+}
+function register($mysql) {
+	$fname = $_POST ['firstname'];
+	$lname = $_POST ['lastname'];
+	$uname = $_POST ['username'];
+	$pwd = $_POST ['password'];
+	
+	if (empty ( $pwd )) {
+		$rank = 4;
+		$msg = "Set a password";
+	}
+	
+	if (empty ( $uname )) {
+		$rank = 3;
+		$msg = "Set a username";
+	}
+	
+	if (empty ( $lname )) {
+		$rank = 2;
+		$msg = "Set a last name";
+	}
+	
+	if (empty ( $fname )) {
+		$rank = 1;
+		$msg = "Set a first name";
+	}
+	if (isset ( $rank )) {
+		$_SESSION ['rank'] = $rank;
+		$_SESSION ['msg'] = $msg;
+		$_SESSION ['form_data'] = array (
+				$fname,
+				$lname,
+				$uname,
+				$pwd 
+		);
+		header ( "location:register.php" );
+		exit ();
+	}
+	
+	$result = $mysql->saveUser ( $fname, $lname, $uname, $pwd );
+	if ($result == 'doublon') {
+		$_SESSION ['rank'] = 3;
+		$_SESSION ['msg'] = 'Username already exists';
+		$_SESSION ['form_data'] = array (
+				$fname,
+				$lname,
+				$uname,
+				$pwd 
+		);
+	} else {
+		$_SESSION ['rank'] = 'top';
+		$_SESSION ['msg'] = 'Registration succeeded';
+	}
+	
+	header ( "location:register.php" );
+	exit ();
+}
+
+include_once '../Vue/footer.inc';
---
+++ Vue/index.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'database/class.User.php';
+include_once 'header.inc';
+
+$msg = isset ( $_SESSION ['msg'] ) ? '<span class="error">*' . $_SESSION ['msg'] . '</span>' : '';
+
+?>
+<form method="post" action="login.php"> 
+<?php if($msg) echo $msg;?>
+  <table>
+		<tr>
+			<td>User:</td>
+			<td><input type="text" name="usr"></td>
+		</tr>
+		<tr>
+			<td>Password:</td>
+			<td><input type="password" name="pwd"></td>
+		</tr>
+		<tr>
+			<td colspan="2" align="right"><input type="submit" name="action"
+				value="Login"></td>
+		</tr>
+	</table> 
+ 
+</form>
+<br />
+<a href="register.php">Register</a>
+ <?php
+		
+if (isset ( $_SESSION ['user'] )) {
+			$user = $_SESSION ['user'];
+			?> 
+&nbsp;&nbsp;&nbsp;<a href="login.php?action=logout">Logout 
+(<?php echo $user->firstname .' '. $user->lastname;?>)</a> 
+<?php }
+
+
+
+include_once 'footer.inc';
+?>
